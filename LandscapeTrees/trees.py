4f26bf612bf2938611bc6e29622fd67b1845c798
scottbing <sbing52@comcast.net>
2020-03-17 20:00:12 -0600

    SB_FlyweightPattern91
    SB_FlyweightPattern91

--- LandscapeTrees/trees.py
+++ LandscapeTrees/trees.py
@@ -2,17 +2,19 @@ class LandscapeTrees(object):
     """Tree Class"""
 
     """default constructor"""
+
     def __init__(self):
         pass
 
     """active constructor"""
+
     def trees(self, tree_name):
         return "LandscapePattern[% s]" % (tree_name)
 
 
-class TreeFamilies(object):
-    """dictionary to store ids of the tree"""
-
+class TreeSpecies(object):
+    
+    """use a dictionary to store the tree information"""
     tree_family = {}
 
     """This is the basis of the Flyweight pattern..."""
@@ -27,15 +29,12 @@ class TreeFamilies(object):
         return id
 
     def set_tree_info(self, tree_info):
-
         """set the tree information"""
         cg = LandscapeTrees()
         self.tree_info = cg.trees(tree_info)
 
     def get_tree_info(self):
-
         """return the tree information"""
-
         return (self.tree_info)
 
 
@@ -46,7 +45,7 @@ if __name__ == '__main__':
 
     """process  the trees"""
     for i in evergreen_tree_data:
-        obj = TreeFamilies(i[0], i[1])
+        obj = TreeSpecies(i[0], i[1])
         obj.set_tree_info(i[2])
         eg_tree_family_objects.append(obj)
 
@@ -56,12 +55,13 @@ if __name__ == '__main__':
         print("id = " + str(id(i)))
         print(i.get_tree_info())
 
+    """Spruce Trees"""
     spruce_tree_data = (('a', 1, 'Blue'), ('a', 2, 'Black Hills'), ('b', 1, 'Norway'))
     sp_tree_family_objects = []
 
     """process  the trees"""
     for i in spruce_tree_data:
-        obj = TreeFamilies(i[0], i[1])
+        obj = TreeSpecies(i[0], i[1])
         obj.set_tree_info(i[2])
         sp_tree_family_objects.append(obj)
 
@@ -70,4 +70,3 @@ if __name__ == '__main__':
     for i in sp_tree_family_objects:
         print("id = " + str(id(i)))
         print(i.get_tree_info())
-
